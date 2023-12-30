b601258562981fa88a38005b9aa35006eb3e0f72
Jahnavi Gogia <jahnavigogia02@gmail.com>
2023-12-31 01:18:16 +0530

    Initializing and drawing the board

--- main.py
+++ main.py
@@ -1,59 +1,118 @@
-import random
 import pygame
+import random
 
+# Initialize Pygame
 pygame.init()
 
-# initial setup
-width = 400
-height = 500
-screen = pygame.display.set_mode([height, width])
-pygame.display.set_caption('2048')
-timer = pygame.time.Clock()
-fps = 60
-font = pygame.font.Font('freesansbold.ttf', 24)
+# Constants
+WIDTH = 400
+HEIGHT = 400
+GRID_SIZE = 4
+TILE_SIZE = WIDTH // GRID_SIZE
+BACKGROUND_COLOR = (187, 173, 160)
+GRID_COLOR = (205, 193, 180)
+FONT_COLOR = (255, 255, 255)
+FONT_SIZE = 36
+SCORE_FONT_SIZE = 24
+SCORE_OFFSET = 10
+FPS = 60
 
+# Directional constants
+UP = (0, -1)
+DOWN = (0, 1)
+LEFT = (-1, 0)
+RIGHT = (1, 0)
 
-# 2048 colour library
-colours = {0: (204, 192, 179),
-           2: (238, 228, 218),
-           4: (237, 224, 200),
-           8: (242, 177, 121),
-           16: (245, 149, 99),
-           32: (246, 124, 95),
-           64: (246, 94, 59),
-           128: (237, 204, 97),
-           256: (237, 204, 114),
-           512: (237, 200, 80),
-           1024: (237, 197, 63),
-           2048: (237, 194, 46),
-           'light-text': (249, 246, 242),
-           'dark text': (119, 110, 101),
-           'other': (0, 0, 0),
-           'bg': (107, 173, 160)}
+# Colors for each tile value (powers of 2)
+TILE_COLORS = {
+    0: (205, 193, 180),
+    2: (238, 228, 218),
+    4: (237, 224, 200),
+    8: (242, 177, 121),
+    16: (245, 149, 99),
+    32: (246, 124, 95),
+    64: (246, 94, 59),
+    128: (237, 207, 114),
+    256: (237, 204, 97),
+    512: (237, 200, 80),
+    1024: (237, 197, 63),
+    2048: (237, 194, 46),
+}
 
+# Function to initialize the game board
+def initialize_board():
+    board = [[0] * GRID_SIZE for _ in range(GRID_SIZE)]
+    add_new_tile(board)
+    add_new_tile(board)
+    return board
 
-board_val = [[0 for _ in range(4) for _ in range(4)]]
+# Function to add a new tile to the board
+def add_new_tile(board):
+   pass
+# Function to draw the game board
+def draw_board(screen, board):
+    screen.fill(BACKGROUND_COLOR)
+    for i in range(GRID_SIZE):
+        for j in range(GRID_SIZE):
+            value = board[i][j]
+            color = TILE_COLORS.get(value, TILE_COLORS[0])
+            pygame.draw.rect(screen, color, (j * TILE_SIZE, i * TILE_SIZE, TILE_SIZE, TILE_SIZE))
+            if value != 0:
+                draw_text(screen, str(value), (j * TILE_SIZE + TILE_SIZE // 2, i * TILE_SIZE + TILE_SIZE // 2))
 
-
-def draw_board():
-    pygame.draw.rect(screen, colours['bg'], [0, 0, 400, 400], 0, 10)
+# Function to draw text on the screen
+def draw_text(screen, text, position):
     pass
 
-# draw pieces for the board
+# Function to draw the score
+def draw_score(screen, score):
+   pass
+
+# Function to move the tiles in a given direction
+def move_tiles(board, direction):
+   pass
 
 
-def draw_pieces(board):
+# Function to check if the game is over
+def is_game_over(board):
+   pass
+# Main function
+def main():
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("2048 Game")
+    clock = pygame.time.Clock()
 
-# main game loop
-run = True
-while run:
-    timer.tick(fps)
-    screen.fill('gray')
-    draw_board()
-    draw_pieces(board_val)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+    board = initialize_board()
+    score = 0
 
-    pygame.display.flip()
-pygame.quit()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    moved = move_tiles(board, UP)
+                elif event.key == pygame.K_DOWN:
+                    moved = move_tiles(board, DOWN)
+                elif event.key == pygame.K_LEFT:
+                    moved = move_tiles(board, LEFT)
+                elif event.key == pygame.K_RIGHT:
+                    moved = move_tiles(board, RIGHT)
+
+                if moved:
+                    add_new_tile(board)
+
+        draw_board(screen, board)
+        draw_score(screen, score)
+        pygame.display.flip()
+
+        if is_game_over(board):
+            print("Game Over! Your score: {}".format(score))
+            pygame.quit()
+            quit()
+
+        clock.tick(FPS)
+
+if __name__ == "__main__":
+    main()
